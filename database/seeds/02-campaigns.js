f15fe24776caa9598478d5360a2f2f9175db11d6
Shomari Roberts <shomariroberts@gmail.com>
2020-10-15 17:39:03 -0400

    updated datetime

--- database/seeds/02-campaigns.js
+++ database/seeds/02-campaigns.js
@@ -7,7 +7,7 @@ exports.seed = function (knex) {
       goal: 5000,
       description:
         'A motion control kit that lets you add tons of movements to your design, camera and light, instantly.',
-      campaign_date: '0000-00-00 00:00:00',
+      campaign_date: '2020-12-25 23:00:00',
       category: 'Design & Tech',
       user_id: 1,
     },
@@ -17,7 +17,7 @@ exports.seed = function (knex) {
       goal: 58719,
       description:
         'Finally redefining the concept of air purifiers thanks to a diffused purification and monitoring system - with powerful nanomaterials.',
-      campaign_date: '0000-00-00 00:00:00',
+      campaign_date: '2020-11-31 23:59:59',
       category: 'Design & Tech',
       user_id: 2,
     },
